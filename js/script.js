b33f4bfe3f0c91a1660657f147ef27f257dc7a72
julianrueda1170 <julianrueda1170@gmail.com>
2020-11-11 16:41:25 -0500

    actualizacion
    errores en menú responsiva
    falta instalar buscador

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.getElementById("move-contenido").classList.toggle('move-contenido-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
+}
