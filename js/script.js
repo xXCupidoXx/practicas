5945b1e0781d98491f58640a1f3c0b7898c1c026
julianrueda1170 <julianrueda1170@gmail.com>
2020-11-12 12:09:10 -0500

    nuevos archivos

--- js/script.js
+++ js/script.js
@@ -5,3 +5,4 @@ function mostrar_menu(){
     document.getElementById("move-contenido").classList.toggle('move-contenido-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
 }
+
